c18a9309bece4477ffccb72516ad83ad29058260
Michael Hayes <hayes.michael89@gmail.com>
2022-02-02 15:26:26 +0200

    skeleton and start of basic components

---
+++ pages/home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "../components/navbar"
+import Footer from "../components/footer"
+function Home() {
+  return <div>
+      <Navbar></Navbar>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, unde? Nesciunt accusamus ullam repudiandae nihil esse ad pariatur dolor. Error veritatis cum incidunt inventore quasi magni harum temporibus, reiciendis odit.</p>
+      <Footer></Footer>
+  </div>;
+}
+
+export default Home;
